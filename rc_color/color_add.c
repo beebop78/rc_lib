5544703603cc5a43be33449aa984037c15edf096
Rodolphe <rcargou@hotmail.fr>
2015-01-16 17:43:47 +0100

    ez

--- rc_color/color_add.c
+++ rc_color/color_add.c
@@ -6,7 +6,7 @@
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/16 14:30:37 by rcargou           #+#    #+#             */
-/*   Updated: 2015/01/16 15:01:36 by rcargou          ###   ########.fr       */
+/*   Updated: 2015/01/16 17:43:23 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ int				color_add(int color1, int color2)
 	int v2;
 	int new;
 
-	v1 = getmix(color1, 'r');
-	v2 = getmix(color2, 'r') + v1;
+	v1 = color_getcomp(color1, 'r');
+	v2 = color_getcomp(color2, 'r') + v1;
 	v2 = v2 > 255 ? 255 : v2;
 	new = v2 << 16;
-	v1 = getmix(color1, 'g');
-	v2 = getmix(color2, 'g') + v1;
+	v1 = color_getcomp(color1, 'g');
+	v2 = color_getcomp(color2, 'g') + v1;
 	v2 = v2 > 255 ? 255 : v2;
 	new += v2 << 8;
-	v1 = getmix(color1, 'b');
-	v2 = getmix(color2, 'b') + v1;
+	v1 = color_getcomp(color1, 'b');
+	v2 = color_getcomp(color2, 'b') + v1;
 	v2 = v2 > 255 ? 255 : v2;
 	new += v2;
 	return (new);
