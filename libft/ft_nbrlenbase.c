96460deed55b381e2cc7dda1ffd4fcf6bf7c6a84
Rodolphe CARGOU <rcargou@e2r3p3.42.fr>
2015-04-03 19:16:47 +0200

    filsdepute

--- libft/ft_nbrlenbase.c
+++ libft/ft_nbrlenbase.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbrbase.c                                    :+:      :+:    :+:   */
+/*   ft_nbrlenbase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcargou <rcargou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/03/23 19:20:04 by rcargou           #+#    #+#             */
-/*   Updated: 2015/03/24 18:38:53 by rcargou          ###   ########.fr       */
+/*   Created: 2015/03/24 18:04:57 by rcargou           #+#    #+#             */
+/*   Updated: 2015/03/24 18:39:21 by rcargou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbrbase(long int n, char *alphabet, int widht)
+int ft_nbrlenbase(long int n, int base)
 {
-	int base;
+	int len;
 
-	widht -= ft_nbrlenbase(n, ft_strlen(alphabet));
-	while (widht-- > 0)
-		write(1, "0", 1);
-	if (n < 0)
+	len = 0 + !n;
+	while (n)
 	{
-		write(1, "-", 1);
-		n *= -1;
+		n = n / base;
+		len++;
 	}
-	base = ft_strlen(alphabet);
-	if (n >= base)
-		ft_putnbrbase(n / base, alphabet, 0);
-	write(1, &(alphabet[n % base]), 1);
+	return (len);
 }
